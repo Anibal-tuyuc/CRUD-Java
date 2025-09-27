dbbf4cb6ff9290964646339c00949172395c08fc
Anibal Tuyuc <juniortuyuc5@gmail.com>
2025-09-26 19:18:22 -0600

    Commit de inicio

---
+++ CRUDJava/src/main/java/com/mycompany/crudjava/CRUDJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudjava;
+
+/**
+ *
+ * @author Admin
+ */
+public class CRUDJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
